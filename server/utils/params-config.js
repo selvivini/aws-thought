bb650da76a14d942539f3d2e1bb283966b9f950c
vinitha <selvivini@gmail.com>
2021-05-20 12:41:07 -0500

    added the bucket policy and modified the ui compoennts to show image when we post an item

--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -7,7 +7,8 @@ const params = fileName => {
     const imageParams = {
       Bucket: 'user-images-cc827a85-e149-4d9c-bc12-b030c2a40bb3',
       Key: `${uuidv4()}.${fileType}`,
-      Body: fileName.buffer
+      Body: fileName.buffer,
+      ACL: 'public-read' // allow read access to this file
     };
   
     return imageParams;
